23c93879c9095124d255e3d87b54cd56a715d3ef
Greg DeVos <gdevos010@gmail.com>
2021-09-03 12:08:59 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Paper 1 for ML7641 at GT. Comparing 5 supervised algorithms on medical datasets',
+    author='Greg DeVos',
+    license='MIT',
+)
