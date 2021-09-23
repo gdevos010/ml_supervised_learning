ba33204cf05207909b13862a98c5eeae2f56bdae
Greg DeVos <gdevos010@gmail.com>
2021-09-23 19:43:21 -0400

    added table export for F1 score and runtime

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+
 from src import fast_run
 from src import models_from_dataset
 from src.data.dataset import Dataset
@@ -17,18 +21,42 @@ def train():
         dataset.load_dataset()
         model_list = models_from_dataset(dataset)
 
+        times = [["Model Type", "Train Time", "Predict Time"]]
+        # scores = [["Model Type", "F1 Train", "F1 Test"]]
+
         # iterate over models
         for model in model_list:
-            model.fit(dataset)
+            train_time = model.fit(dataset)
 
-            model.score(dataset, train=True)
-            model.score(dataset, train=False)
+            f1_train, run_time_train = model.score(dataset, train=True)
+            f1_test, run_time_test = model.score(dataset, train=False)
 
             if not fast_run:
                 model.tune(dataset)
 
-                model.score(dataset, train=True)
-                model.score(dataset, train=False)
+                f1_train, run_time_train = model.score(dataset, train=True)
+                f1_test, run_time_test = model.score(dataset, train=False)
+
+            times.append([model.title, train_time, round(run_time_test, 3)])
+        save_table(times, dataset)
+
+
+def save_table(results, dataset: Dataset):
+    fig, ax = plt.subplots()
+    table = ax.table(cellText=results, loc='center')
+
+    # modify table
+    table.set_fontsize(14)
+    table.scale(1, 4)
+    ax.axis('off')
+
+    # display and save table
+    project_dir = Path(__file__).resolve().parents[2]
+    output_path = Path.joinpath(project_dir, "reports", "figures", dataset.name)
+    output_path.mkdir(parents=True, exist_ok=True)
+    filepath = Path.joinpath(output_path, "run_times.png")
+    fig.savefig(filepath)
+    plt.show()
 
 
 if __name__ == '__main__':
