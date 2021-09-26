8ca7818ff533e7c88360722620cd930ba025d591
Greg DeVos <gdevos010@gmail.com>
2021-09-26 07:07:23 -0400

    loss curve for NNs

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,8 +1,11 @@
 import multiprocessing as mp
+import secrets
 from pathlib import Path
 
 import numpy as np
 from matplotlib import pyplot as plt
+from sklearn.model_selection import ParameterGrid
+from sklearn.neural_network import MLPClassifier
 from yellowbrick.model_selection import LearningCurve
 from yellowbrick.model_selection import ValidationCurve
 
@@ -14,9 +17,74 @@ from src.utils.logger import info
 from src.utils.logger import init_logger
 
 
+def draw_result(lst_iter, lst_loss, lst_acc, title):
+    plt.plot(lst_iter, lst_loss, '-b', label='loss')
+    plt.plot(lst_iter, lst_acc, '-r', label='accuracy')
+
+    plt.xlabel("n iteration")
+    plt.legend(loc='upper left')
+    plt.title(title)
+
+    plt.savefig(title + ".png")  # should before show method
+    plt.show()
+
+
 def loss_curve():
-    # TODO
-    pass
+    project_dir = Path(__file__).resolve().parents[2]
+
+    datasets = get_datasets()
+
+    for ds_cnt, filename in enumerate(datasets):
+        dataset = Dataset(filename)
+        info(f"dataset: {dataset.name}")
+
+        dataset.load_dataset()
+        model_list = models_from_dataset(dataset)
+
+        output_path = Path.joinpath(project_dir, "reports", "figures", dataset.name)
+        output_path.mkdir(parents=True, exist_ok=True)
+
+        # iterate over classifiers
+        for model in model_list:
+            # only run on NN
+            if "Neural Network" not in model.title:
+                continue
+
+            # skip if visualization already exists
+            filepath = Path.joinpath(output_path, f"{model.title} loss curve.png")
+            if filepath.is_file():
+                info(f"viz exists. skipping: {filepath}")
+                continue
+
+            results = dict()
+            param_grid = list(ParameterGrid(model.validation_curve))
+
+            secure_random = secrets.SystemRandom()
+            num_to_select = 6
+            param_grid = secure_random.sample(param_grid, num_to_select)
+
+            for params in param_grid:
+                model_ = MLPClassifier(max_iter=model.max_iter,
+                                       momentum=.75,
+                                       hidden_layer_sizes=(50, 25, 10),
+                                       early_stopping=True,
+                                       n_iter_no_change=15,
+                                       **params)
+                model_.fit(dataset.x_train, dataset.y_train)
+
+                results[f'a: {params["alpha"]}'.ljust(12) + f' lr: {params["learning_rate_init"]}'] = model_.loss_curve_
+
+            # plot loss curves
+            plt.xlabel("iterations")
+            plt.ylabel("Loss")
+            plt.title(f"{model.title} Loss Curve ({dataset.name})")
+            for label, data in results.items():
+                plt.plot(data, label=label)
+            plt.legend()
+            plt.savefig(filepath)
+            plt.show()
+            plt.clf()
+            plt.close()
 
 
 # https://www.scikit-yb.org/en/latest/api/model_selection/validation_curve.html
@@ -55,7 +123,7 @@ def validation_curve():
                 viz.fit(dataset.x, dataset.y)
                 best_test_scores = np.argmax(viz.test_scores_mean_)
                 info(f"best {param_name} is {param_range[best_test_scores]} with a score of"
-                     f" {round(viz.test_scores_mean_[best_test_scores],3)}")
+                     f" {round(viz.test_scores_mean_[best_test_scores], 3)}")
 
                 viz.show(outpath=filepath)
                 viz.show(clear_figure=True)
@@ -66,6 +134,7 @@ def validation_curve():
 def learning_curve():
     project_dir = Path(__file__).resolve().parents[2]
     sizes = np.arange(0.3, 1., .2)
+    sizes = np.linspace(0.3, 1., 6)
 
     datasets = get_datasets()
 
@@ -81,8 +150,8 @@ def learning_curve():
 
         # iterate over classifiers
         for model in model_list:
-            # skip if visualization already exists
             filepath = Path.joinpath(output_path, f"{model.title} learning curve.png")
+            # skip if visualization already exists
             if filepath.is_file():
                 info(f"viz exists. skipping: {filepath}")
                 continue
@@ -97,8 +166,8 @@ def learning_curve():
             viz.fit(dataset.x, dataset.y)
 
             best_test_scores = np.argmax(viz.test_scores_mean_)
-            info(f"best split is {round(sizes[best_test_scores],3)} with a score of "
-                 f"{round(viz.test_scores_mean_[best_test_scores],3)}")
+            info(f"best split is {round(sizes[best_test_scores], 3)} with a score of "
+                 f"{round(viz.test_scores_mean_[best_test_scores], 3)}")
 
             viz.show(outpath=filepath)
             viz.show(clear_filename=True)
@@ -162,12 +231,10 @@ def gen_plots():
     visualize_dataset()
     validation_curve()
     learning_curve()
+    loss_curve()
 
 
 if __name__ == '__main__':
     init_logger()
 
     gen_plots()
-
-# https://stackoverflow.com/questions/46912557/is-it-possible-to-get-test-scores-for-each-iteration-of-mlpclassifier
-# https://stackoverflow.com/questions/52349169/plotting-test-valid-and-train-acc-again-epochs-in-sklearn
